847e7c546873daab1ca893489bbeb945bbd6ae7a
dislersd <dislersd@gmail.com>
2020-05-21 21:45:09 -0700

    removing vuetify with npm - will add with "vue add"

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <v-app>
-
+    <Login />
   </v-app>
 </template>
 
 <script>
-
-
+import Login from './components/Auth/Login'
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-
-  }
-}
+    Login,
+  },
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import "./plugins/vuetify.js"
+// import "./plugins/vuetify.js"
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
